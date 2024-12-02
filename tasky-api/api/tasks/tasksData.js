5544d373f15b680c5c0ae83a7cd5486229c2ddfc
35389 <20098764@mail.wit.ie>
2024-12-02 23:54:44 +0000

    added routing for posting a new task

---
+++ tasky-api/api/tasks/tasksData.js
@@ -0,0 +1,12 @@
+export const tasksData = {
+    page: 1,
+    total_pages: 1,
+    total_results: 3,
+    tasks: [{
+        "title": "Complete Assignment 1",
+        "description":"Finish assignment 1 for WAD2",
+        "deadline":"tomorrow",
+        "priority":"medium",
+        "done":false
+    }]
+};
