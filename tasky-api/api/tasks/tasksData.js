1b717d2847ab4a68b331dc854e8fde66f9934fdb
35389 <20098764@mail.wit.ie>
2024-12-03 00:11:52 +0000

    Express Lab - exercise done

--- tasky-api/api/tasks/tasksData.js
+++ tasky-api/api/tasks/tasksData.js
@@ -1,12 +1,17 @@
 export const tasksData = {
     page: 1,
     total_pages: 1,
-    total_results: 3,
-    tasks: [{
-        "title": "Complete Assignment 1",
-        "description":"Finish assignment 1 for WAD2",
-        "deadline":"tomorrow",
-        "priority":"medium",
-        "done":false
-    }]
+    total_results: 1,
+    tasks: [
+        {
+            id: "1", // Unique identifier
+            title: "Complete Assignment 1",
+            description: "Finish assignment 1 for WAD2",
+            deadline: "tomorrow",
+            priority: "medium",
+            done: false,
+            created_at: new Date().toISOString(),
+            updated_at: new Date().toISOString(),
+        },
+    ],
 };
