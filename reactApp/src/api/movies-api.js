432bd719090560c59e1e161a8c385ff9ec4641ca
35389 <20098764@mail.wit.ie>
2025-01-06 00:12:58 +0000

    Integration Lab - Get Movies from API

--- reactApp/src/api/movies-api.js
+++ reactApp/src/api/movies-api.js
@@ -1,9 +1,16 @@
 export const getMovies = async () => {
-    const response = await  fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=4939bdef6fbeb7eb81e0f8c7ffd45d55&language=en-US&include_adult=false&page=1`
-    )
-    return response.json()
-  };
+  const response = await fetch('http://localhost:8080/api/movies', {
+      headers: {
+          'Authorization': `Bearer ${localStorage.getItem('token')}`
+      }
+  });
+  if (!response.ok) {
+      throw new Error('Network response was not ok');
+  }
+  return response.json();
+};
+
+
 
   export const login = async (username, password) => {
     const response = await fetch('http://localhost:8080/api/users', {
